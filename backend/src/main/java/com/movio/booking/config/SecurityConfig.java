ece539ab4a112a97c2a172e46b3055829ee6b47b
CoderAditya04 <adtag209@gmail.com>
2026-08-10 19:04:56 +0530

    Add Elastic IP to CORS allowed origins

--- backend/src/main/java/com/movio/booking/config/SecurityConfig.java
+++ backend/src/main/java/com/movio/booking/config/SecurityConfig.java
@@ -53,7 +53,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration config = new CorsConfiguration();
-        config.setAllowedOrigins(List.of("http://localhost:5173","http://15.252.136.190"));
+        config.setAllowedOrigins(List.of("http://localhost:5173","http://15.252.160.141"));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
         config.setAllowCredentials(true);
